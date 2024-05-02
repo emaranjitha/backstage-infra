3ede33045446be041706eb7a5c033e0d100aed6d
emaranjitha <ranjithar427@gmail.com>
2024-05-03 02:25:09 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
